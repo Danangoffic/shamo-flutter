31b4e9ee1297f4a07c7b7f4a2f2efd7d597bdc09
danangoffic <darifrahmanda@gmail.com>
2022-09-30 21:11:37 +0700

    init flutter

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(body: Image.asset('assets/image_splash.png'));
+    return scaffold;
+  }
+}
